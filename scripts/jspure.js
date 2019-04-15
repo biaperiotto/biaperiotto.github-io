6025ffb9fdb8f9888ee89c05edea80bdd235b490
lsilvatti <leosilvatti0@gmail.com>
2019-04-15 17:11:23 -0300

    imagens colocadas, galeria feita

--- scripts/jspure.js
+++ scripts/jspure.js
@@ -1,14 +1,3 @@
 $(".rotate").click(function () {
     $(this).toggleClass("down");
 })
-
-$(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $("nav").addClass("changeColor");
-        }
-        if ($(this).scrollTop() < 50) {
-            $("nav").removeClass("changeColor");
-        }
-    });
-});
